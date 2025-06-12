c770810bcff3d60eb18b217b89fcf7fb5c90ed9e
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-12 09:06:53 +0200

    Ajout du juste prix

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let objectif = Math.floor(Math.random() * 100) + 1;
+let joueur = -1
+let nbEssaie = 0
+
+alert("Bienvenue au JustePrix, veuillez deviner le prix que je pense entre 1 et 100")
+
+while (joueur !== objectif) {
+    joueur = +prompt("Veuillez entrer le nombre que vous pensez être bon")
+    if (joueur > objectif) {
+        alert("C'est moins !")
+    } else if (joueur < objectif) {
+        alert("C'est plus !")
+    }
+    nbEssaie++
+}
+
+if (nbEssaie === 1) {
+    alert(`Wouah du premier coup ! C'était bien ${objectif} le prix que je pensais !`)
+} else {
+    alert(`Félicitation, vous avez devinez le prix de ${objectif} en ${nbEssaie} essaies.`)
+}
+
+if (window.confirm("Voulez vous refaire une partie ?")) {
+    window.location.reload(true);
+} else {
+    alert("Au revoir")
+}
